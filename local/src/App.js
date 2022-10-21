3d29758b8ec19a3286c41c67d406433b83b40c5f
jirapun <jirapun090@gmail.com>
2022-10-21 11:45:23 +0700

    first class

---
+++ local/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import RestBasic from './rest-basic';
+import FormGet from './form-get';
+
+function App() {
+  return (
+    <div className="App">
+      <RestBasic />
+      <FormGet />
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express()
+const port = 8000
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.static('public'))
+
+app.get('/api/server-time', (req, res) => {
+    let now = new Date()
+    let time = {
+        hour: now.getHours(),
+        minute: now.getMinutes(),
+        second: now.getSeconds()
+    }
+    res.json(time)
+})
+
+function rd(min, max){
+    let x = (max - min) + 1
+    return min + Math.floor(Math.random() * x )
+}
+
+app.get('/api/football-result', (req, res) => {
+    let table = `
+        <table border="1" style="margin: 7px auto">
+        <tr><td>ManU</td><td>${rd(0,5)}-${rd(0,5)}</td><td>Liverpool</td></tr>
+        <tr><td>Chelsea</td><td>${rd(0,5)}-${rd(0,5)}</td><td>ManCity</td></tr>
+        <tr><td>Arsenal</td><td>${rd(0,5)}-${rd(0,5)}</td><td>Spur</td></tr>
+    `
+    res.send(table)
+})
+
+app.get('/api/form-get', (req, res) => {
+    let t = req.query.target || ''
+    let k = req.query.kw || ''
+    let n = parseInt((Math.random()* 1000))
+    let r = {
+        target: t,
+        kw: k,
+        results: n
+    }
+    res.json(r)
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}!`)
+})
